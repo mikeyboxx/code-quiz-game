c96a2131ecd6a7235c6c66b8ad084be217579358
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-04 00:34:49 -0400

    Added thr question.js file containing the quiz questions and answers

---
+++ assets/js/questions.js
@@ -0,0 +1,25 @@
+// an array of objects containing question, choices, and indwex of answer
+var quiz = [
+    {
+        question: 'If you type the following code in the console window, what result will you get?\n3 > 2 > 1 === false;',
+        choices: ['true', 'false'],
+        answer: 0
+    },
+    {
+        question: 'JavaScript is a ___ -side programming language.',
+        choices: ['Client', 'Server', 'Both', 'None'],
+        answer: 2
+    },
+    {
+        question: 'Which of the following will write the message “Hello DataFlair!” in an alert box?',
+        choices: ['alertBox(“Hello DataFlair!”);', 'alert(Hello DataFlair!);', 'msgAlert(“Hello DataFlair!”);', 'alert(“Hello DataFlair!”);'],
+        answer: 3
+    },
+    {
+        question: 'How do you find the minimum of x and y using JavaScript?',
+        choices: ['min(x,y);', ' Math.min(x,y)', 'Math.min(xy)', 'min(xy);'],
+        answer: 1
+    },
+];
+
+
