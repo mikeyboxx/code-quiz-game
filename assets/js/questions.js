16976929528b590705b961acc6fdd34625ee2b9a
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-05 17:45:06 -0400

    Added more quiz questions to questions.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -17,9 +17,44 @@ var quiz = [
     },
     {
         question: 'How do you find the minimum of x and y using JavaScript?',
-        choices: ['min(x,y);', ' Math.min(x,y)', 'Math.min(xy)', 'min(xy);'],
+        choices: ['min(x,y);', 'Math.min(x,y)', 'Math.min(xy)', 'min(xy);'],
         answer: 1
     },
+    {
+        question: 'Which built-in method calls a function for each element in the array?',
+        choices: ['while()', ' loop()', 'forEach()', 'None of the above'],
+        answer: 2
+    },
+    {
+        question: 'Which built-in method returns the calling string value converted to upper case?',
+        choices: ['toUpperCase()', 'toUpper()', 'changeCase(case)', 'None of the above'],
+        answer: 0
+    },
+    {
+        question: 'Which of the following function of Boolean object returns the primitive value of the Boolean object?',
+        choices: ['toSource()', 'valueOf()', 'toString()', 'None of the above'],
+        answer: 1
+    },
+    {
+        question: 'Which of the following function of String object executes the search for a match between a regular expression and a specified string?',
+        choices: ['concat()', 'match()', 'replace()', 'search()'],
+        answer: 3
+    },
+    {
+        question: 'Which of the following function of String object creates a string to be displayed as bold as if it were in a <b> tag?',
+        choices: ['anchor()', 'big()', 'blink()', 'bold()'],
+        answer: 3
+    },
+    {
+        question: 'Which of the following function of Array object returns the first (least) index of an element within the array equal to the specified value, or -1 if none is found?',
+        choices: ['indexOf()', 'join()', 'lastIndexOf()', 'map()'],
+        answer: 0
+    },
+    {
+        question: 'Which of the following function of Array object returns true if at least one element in this array satisfies the provided testing function?',
+        choices: ['reverse()', 'shift()', 'slice()', 'some()'],
+        answer: 3
+    }
 ];
 
 
